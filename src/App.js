adc230138ed88ba220832181752f3c3215afbbda
bashar4980 <sagor.ahmed3669@gmail.com>
2022-10-15 15:42:02 +0600

    Firebase google authentication is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
+import "./App.css";
+import app from "./firebase/firebase.init";
 
+const auth = getAuth(app);
 function App() {
+  const [userInfo, setUserInfo] = useState({});
+  const provider = new GoogleAuthProvider();
+  const sigInGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUserInfo(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const logOut= () =>{
+      signOut(auth)
+      .then(()=>{
+        alert("Successfully log out");
+        setUserInfo({})
+      })
+      .cathch(error =>{
+        console.error(error.message)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am Firebase</h1>
+      {userInfo.uid ? (
+        <button onClick={logOut}>Log out</button>
+      ) : (
+        <button onClick={sigInGoogle}>Sign Up</button>
+      )}
+
+      <div>
+       {
+        userInfo.uid &&  <>
+        <h1>Name: {userInfo.displayName}</h1>
+        <h3>Email: {userInfo.email}</h3>
+        <img src={userInfo.photoURL} alt="user"></img>
+        <h4>uid: {userInfo.uid}</h4>
+        </>
+       }
+      </div>
     </div>
   );
 }
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLoBhjmk26GjSxxGie-0A223zv-e4ErAM",
+  authDomain: "second-basic-firebase.firebaseapp.com",
+  projectId: "second-basic-firebase",
+  storageBucket: "second-basic-firebase.appspot.com",
+  messagingSenderId: "463714348145",
+  appId: "1:463714348145:web:f0dd458f0bdf5dffc57fd0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
